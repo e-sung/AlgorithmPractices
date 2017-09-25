0931a65d7bbca27789268cfae41e482f606456f6
e-sung <dev.esung@gmail.com>
2017-09-25 20:44:00 +0900

    Create overTimeNumber.js

---
+++ JavaScript/overTimeNumber.js
@@ -0,0 +1,21 @@
+function noOvertime(no, works) { 
+	while(no>0){
+		let largestNumberInWorks = Math.max.apply(Math,works);
+		let largestNumberIndex = works.indexOf(largestNumberInWorks);
+		works[largestNumberIndex] -= 1;
+		no -= 1;
+	}
+	
+
+	return getOverTimeNumber(works);
+}
+
+function getOverTimeNumber(works){
+	var answer = 0;
+	for(var i=0, item; item = works[i]; i++){
+		answer += item*item;
+	}
+	return answer;
+}
+
+console.log(noOvertime(4,[4,3,3]))
